f86183b170e097b17bbe04e97723ee968f3dbf77
Luis Borjas <luisfborjas@gmail.com>
2019-06-10 03:22:04 -0400

    sort of rendering a scale

--- src/Render.hs
+++ src/Render.hs
@@ -8,20 +8,31 @@ import Scales
 import Chords
 import Data.Ratio
 import Data.Char (toLower)
+import Data.List (intercalate)
+import System.Process (callCommand)
+
+type ScoreFragment = String
 
 -- For reference, start here: http://lilypond.org/text-input.html
-
+-- and: http://lilypond.org/doc/v2.18/Documentation/learning/simple-notation#all-together
 class LilypondRenderable a where
-  toLilypond :: a -> String
+  toLilypond :: a -> ScoreFragment
 
 instance LilypondRenderable Chord where
   toLilypond c = "<>"
+
+lconcat = intercalate " "
+
+-- Allows us to render a subset of possible music values:
+-- e.g.
+-- λ> toLilypond $ removeZeros $ Scales.toMusic en cMaj
+-- "c'8 d'8 e'8 f'8 g'8 a'8 b'8 c''8"
 
 instance LilypondRenderable (Music Pitch) where
   toLilypond (Prim (Note d p)) = concat [toLilypond p, toLilypond d]
   toLilypond (Prim (Rest 0)) = "s"
   toLilypond (Prim (Rest d)) = "r" ++ toLilypond d
-  toLilypond (m1 :+: m2) = concat [toLilypond m1, toLilypond m2]
+  toLilypond (m1 :+: m2) = lconcat [toLilypond m1, toLilypond m2]
 
 instance LilypondRenderable Pitch where
   toLilypond (pc, o) = concat [toLilypond pc, toLilypond o]
@@ -42,5 +53,41 @@ instance LilypondRenderable Octave where
     | o < 3  = concat $ take (3-o) $ repeat ","
     | o > 3  = concat $ take (o-3) $ repeat "'"
 
--- renderScale :: Scale -> String
--- renderScale s = mconcat $ mMap toLilypond $ Scales.toMusic en s
+-- TODO: there's many more bells and whistles, like scales and tempi:
+-- http://lilypond.org/doc/v2.18/Documentation/learning/simple-notation#all-together
+renderScore :: ScoreFragment -> String
+renderScore sf = "{" ++ sf ++ "}"
+
+renderScoreFile :: String -> String -> IO ()
+renderScoreFile s n = do
+  callCommand $ concat ["echo ", s, " > ", n, ".ly"]
+  callCommand $ concat ["~/bin/lilypond ", n, ".ly"]
+
+{-
+
+sorta working:
+
+λ> cMaj = minorScale (A,4)
+λ> s = renderScore $ toLilypond $ removeZeros $ Scales.toMusic en cMaj
+λ> renderScoreFile s "a-minor"
+GNU LilyPond 2.18.2
+Processing `a-minor.ly'
+Parsing...
+a-minor.ly:1: warning: no \version statement found, please add
+
+\version "2.18.2"
+
+for future compatibility
+Interpreting music...
+Preprocessing graphical objects...
+Finding the ideal number of pages...
+Fitting music on 1 page...
+Drawing systems...
+Layout output to `a-minor.ps'...
+Converting to `./a-minor.pdf'...
+Success: compilation successfully completed
+λ> s
+"{a'8 b'8 c''8 d''8 e''8 f''8 g''8 a''8}"
+λ> 
+
+-}
